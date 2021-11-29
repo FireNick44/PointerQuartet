00609cf94746608aa401292698fc8c7b7a068334
FireNick44 <yannic.studer44@gmail.com>
2021-11-29 11:22:12 +0100

    x

--- Main.cpp
+++ Main.cpp
@@ -27,5 +27,20 @@ int random(int min, int max) {
 }
 
 void ausgabe() {
-  printf("Was Möchten Sie Vergleichen?\n");
+
+  char c;
+  printf("\n\n\n");
+  printf("==================================\n");
+  printf("Was M\x94""chten Sie Vergleichen?\n");
+  printf("1 = Variable 1: value X... \n");
+  printf("2 = Variable 2:\n");
+  printf("3 = Variable 3:\n");
+  printf("4 = Variable 4:\n");
+  printf("==================================\n");
+  printf("5 = Neu Starten\n");
+  printf("6 = Beenden\n");
+
+  c = getchar();
+
+
 }
